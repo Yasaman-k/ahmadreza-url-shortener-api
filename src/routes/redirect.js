b757264bfa8d1ffe0061a2b82462e429f53093b3
AhmadReza Ataee <ahmadr.ataee@gmail.com>
2021-10-03 19:45:34 +0330

    feat: add controller for get urlShortened and code for redirect tu full url

--- src/routes/redirect.js
+++ src/routes/redirect.js
@@ -1,3 +1,16 @@
-const { Router } = require('express');
+const router = require('express').Router();
+const { Logger } = require('@lo-agency/logger');
+const Urls = require('../database/urls-repository');
 
-module.exports = Router;
+router
+    .get('/:shortUrl', (req, res) => {
+        let url = Urls.getUrlCode(req.params.shortUrl)
+        if (!url) {
+            res.status(404).send({ error: 'Not Founded' });
+            return;
+        }
+        res.redirect(/^(http|https)/.test(url.full) ? url.full : "http://" + url.full)
+        Urls.updateViews(url.id)
+    })
+
+module.exports = router;
