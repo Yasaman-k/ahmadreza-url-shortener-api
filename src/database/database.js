26b033eae9af4caf239df5efe8e19bc22eb28675
AhmadReza Ataee <ahmadr.ataee@gmail.com>
2021-10-03 21:06:00 +0330

    feat: add db-setup flag in run command project for run and execute sql codes in migrations, used for first run project

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,9 @@
 const fs = require('fs');
 const { Logger } = require('@lo-agency/logger');
 const betterSqlite3 = require('better-sqlite3');
+const db = new betterSqlite3(process.env.DB_PATH || `${__dirname}/main.db`, { verbose: (message) => Logger.info(`DB-QUERY: ${message}`) });
 
-const dbPath = process.env.DB_PATH || `${__dirname}/main.db`;
-let db = undefined;
-
-if (fs.existsSync(dbPath)) {
-    db = new betterSqlite3(dbPath);
-} else {
-    db = new betterSqlite3(dbPath);
+if (process.argv.includes('db-setup')) {
     fs.readFile(`${__dirname}/migrations.sql`, { encoding: 'utf-8' }, (error, data) => {
         if (error) {
             Logger.error(error.message);
