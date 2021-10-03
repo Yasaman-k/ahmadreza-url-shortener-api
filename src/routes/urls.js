270cd3aeb2172d4715fa6383752569ea35619f3b
AhmadReza Ataee <ahmadr.ataee@gmail.com>
2021-10-03 19:44:13 +0330

    feat: add controllers urls route, insert data and get all data for post and get

--- src/routes/urls.js
+++ src/routes/urls.js
@@ -1,3 +1,23 @@
-const { Router } = require('express');
+const router = require('express').Router();
+const { Logger } = require('@lo-agency/logger');
+const Urls = require('../database/urls-repository');
 
-module.exports = Router;
+router
+    .route('/api/urls')
+    .post((req, res) => {
+        let url = req.body.url
+        if (!validUrl(url)) {
+            res.status(400).send({ error: 'check url' });
+            return;
+        }
+        res.status(200).send(Urls.insertUrl(url))
+    })
+    .get((req, res) => {
+        res.status(200).send(Urls.getAllUrls())
+    })
+
+function validUrl(url) {
+    return /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/.test(url)
+}
+
+module.exports = router;
