d7dc7f318ffbf4ce20b5ede5fff065804cda9724
AhmadReza Ataee <ahmadr.ataee@gmail.com>
2021-10-03 18:42:02 +0330

    feat: add database connection and setup migration script for execute sql codes in migrations.sql file

---
+++ src/database/database.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const { Logger } = require('../helpers/utils');
+const betterSqlite3 = require('better-sqlite3');
+
+const dbPath = process.env.DB_PATH || `${__dirname}/main.db`;
+let db = undefined;
+
+if (fs.existsSync(dbPath)) {
+    db = new betterSqlite3(dbPath);
+} else {
+    db = new betterSqlite3(dbPath);
+    fs.readFile(`${__dirname}/migrations.sql`, { encoding: 'utf-8' }, (error, data) => {
+        if (error) {
+            Logger.error(error.message);
+            return;
+        }
+        db.exec(data);
+    })
+}
+
+process.on('exit', () => db.close())
+
+module.exports = db;
