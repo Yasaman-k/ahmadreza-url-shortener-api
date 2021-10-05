70b7976100353089d2619b7e6ea586697048eb31
AhmadReza Ataee <ahmadr.ataee@gmail.com>
2021-10-05 19:44:12 +0330

    refactor: improve validate url regex

--- src/routes/urls.js
+++ src/routes/urls.js
@@ -29,7 +29,7 @@ router
     })
 
 function validUrl(url) {
-    return /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/.test(url);
+    return /^(https?:\/\/)?(\w+[\-\.@]?\w+)+(\.\w+)+([\/\?:][\w%#@\/\?\-_~\.,\+\=\$&'\(\)\*;\:\[\]!]*)?$/.test(url);
 }
 
 module.exports = router;
