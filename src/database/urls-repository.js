9b0809f3d11c40ff0cd90b360ed33f6d01463f9d
AhmadReza Ataee <ahmadr.ataee@gmail.com>
2021-10-16 20:57:10 +0330

    refactor: improve all function for get all data and filtered option, delete Additional functions

--- src/database/urls-repository.js
+++ src/database/urls-repository.js
@@ -3,15 +3,15 @@ const db = require('./database');
 const table = 'urls';
 
 /**
- // Function for insert url and record in urls table from db
+ // function for insert url and record in urls table from db
  * @param {String} fullUrl - full url from request body for shortened 
  * @param {Number} codeUrlLength - random code and string length for url as shortened, defaul is 5
  * @return {Object} - this function return row or record object inserted
  */
-const insertUrl = (fullUrl, codeUrlLength = 5) => {
-    const randomStr = Math.random().toString(36).slice(2, (+process.env.SHORT_URL_LENGTH || codeUrlLength) + 2);
+const insert = (fullUrl) => {
+    const randomStr = Math.random().toString(36).slice(2, (+process.env.SHORT_URL_LENGTH || 5) + 2);
     try {
-        return getUrlId(
+        return db.prepare(`SELECT * FROM ${table} WHERE id=?`).get(
             db.prepare(` INSERT INTO ${table} (full, short) VALUES (?, ?)`).run(
                 fullUrl,
                 randomStr
@@ -19,7 +19,7 @@ const insertUrl = (fullUrl, codeUrlLength = 5) => {
         )
     } catch (error) {
         if (error.message === 'UNIQUE constraint failed: urls.short') {
-            insertUrl(fullUrl, codeUrlLength);
+            insert(fullUrl);
         } else {
             throw new Error(error);
         }
@@ -27,44 +27,64 @@ const insertUrl = (fullUrl, codeUrlLength = 5) => {
 }
 
 /**
- * Function for get all rows from urls table in db
- * @return {Array} - this function return collection as an array include record objects
+ * function for get all filtered rows from urls table in db 
+ * @param {Object} - this function takes an object parameter containing the approved keys to filter the record and all available urls.
+ * @return {Object} - this function return collection as an array include filtered record objects
  */
-const getAllUrls = () => {
-    return db.prepare(`SELECT * FROM ${table}`).all();
+const all = ({ filter = '', search = '', order = 'id', sort = 'asc', limit = -1, part = 0 }) => {
+
+    let query = '';
+    switch (filter) {
+        case 'most-sites':
+            query = `SELECT COUNT(full) AS count,full AS url,SUM(views) AS views FROM ${table}`;
+            break;
+        default:
+            query = `SELECT * FROM ${table}`;
+            break;
+    }
+
+    let data = {};
+    query += ` WHERE full LIKE @search ${filter ? 'GROUP BY full' : ''} ORDER BY ${order} ${sort.toUpperCase()}`;
+    data = db.prepare(query).all({ search: `%${search}%` });
+    let total = data.length;
+    let parts = Math.ceil(total / limit);
+
+    if (limit >= 0) {
+        query += ` LIMIT @limit OFFSET @part`;
+        data = db.prepare(query).all({ search: `%${search}%`, limit: limit, part: limit * part });
+    }
+
+    const result = {
+        urls: data,
+        total: total,
+        limit: limit < 0 ? null : limit,
+        parts: parts < 0 ? null : parts
+    }
+
+    return result;
 }
 
 /**
- * Function for get rows or record from urls table in db by url shortened code
- * @param {string} urlCode - url code (unique string) for get row
+ * function for get rows or record from urls table in db by url id or short code
+ * @param {Number|String} codeId - url code (unique string) or url id for get row
  * @return {Object} - this function return row or record object inserted
  */
-const getUrlCode = (urlCode) => {
-    return db.prepare(`SELECT * FROM ${table} WHERE short=?`).get(urlCode);
+const get = (codeId) => {
+    return db.prepare(`SELECT * FROM ${table} WHERE id =@codeId OR short =@codeId`).get({ codeId: codeId });
 }
 
 /**
- * Function for get rows or record from urls table in db by url id
- * @param {Number} urlCode - url code (unique string) for get row
- * @return {Object} - this function return row or record object inserted
- */
-const getUrlId = (urlId) => {
-    return db.prepare(`SELECT * FROM ${table} WHERE id=?`).get(urlId);
-}
-
-/**
- * Function for get rows or record from urls table in db by url code
+ * function for get rows or record from urls table in db by url code
  * @param {Number} urlCode - url id for get row
  * @return {Object} - this function return object as changes result
  */
 const updateViews = (urlId) => {
-    return db.prepare(`UPDATE ${table} SET views=views+1 WHERE id=?`).run(urlId);
+    return db.prepare(`UPDATE ${table} SET views = views + 1 WHERE id =? `).run(urlId);
 }
 
 module.exports = {
-    insertUrl,
-    getAllUrls,
-    getUrlCode,
-    getUrlId,
-    updateViews
+    insert,
+    all,
+    get,
+    updateViews,
 };
